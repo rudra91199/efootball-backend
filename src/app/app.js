c6f04d777bd5168c960b4a4520622322a18f2f6f
rudra91199 <rskrrudra9111999@gmail.com>
2025-10-11 01:52:20 +0600

    cors error

--- src/app/app.js
+++ src/app/app.js
@@ -8,7 +8,12 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://efootball-center.netlify.app/",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
