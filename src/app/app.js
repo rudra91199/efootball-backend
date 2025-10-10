374d132aac75f5ffafea00295a45f311832ed582
rudra91199 <rskrrudra9111999@gmail.com>
2025-10-11 03:11:15 +0600

    increased payload limit

--- src/app/app.js
+++ src/app/app.js
@@ -8,10 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 
 // List of allowed origins
 const allowedOrigins = [
