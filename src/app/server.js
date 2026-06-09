3b446006378b367f8a2aecc4bcb58a8cb0f5945b
rudra91199 <rskrrudra9111999@gmail.com>
2026-06-10 01:42:32 +0600

    server production

--- src/app/server.js
+++ src/app/server.js
@@ -12,7 +12,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4", "1.1.1.1"]);
 
 const connection = async () => {
   try {
-    await mongoose.connect(db_url_development);
+    await mongoose.connect(db_url_production);
     server = app.listen(config.port, () => {
       console.log(`Server is running on port ${config.port}`);
     });
